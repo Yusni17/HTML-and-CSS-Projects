9653b54d3accaf9ee5c6d286d0f40df212e14e56
Yusni17 <itsyusniev@gmail.com>
2026-06-18 22:10:44 +0100

    Basic HTML and Javascript 1 Assignment

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,8 @@
+alert("Hello,World");
+
+function My_First_Function() {
+    var string = "Kiss me, I'm Bulgarian!";
+    var result = string.fontcolor("green");
+    document.getElementById("Bulgarian").innerHTML = result;
+
+}
