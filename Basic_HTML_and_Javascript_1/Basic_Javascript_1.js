8d61950c6a9307ffdd8ca3410bf3a9dfcf43ffb4
Yusni17 <itsyusniev@gmail.com>
2026-06-18 22:35:47 +0100

    Remove folder

--- Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
+++
@@ -1,8 +0,0 @@
-alert("Hello,World");
-
-function My_First_Function() {
-    var string = "Kiss me, I'm Bulgarian!";
-    var result = string.fontcolor("green");
-    document.getElementById("Bulgarian").innerHTML = result;
-
-}
